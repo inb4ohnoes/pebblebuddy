0979c0e3eaa30fc00f1ed15d6b063ffd8c9b6cc3
brandonxia01 <brandonxia01@gmail.com>
2014-10-18 03:25:44 -0400

    time + weather
    n shit

--- src/weather.c
+++ src/weather.c
@@ -1,5 +1,8 @@
 #include "pebble.h"
-
+#include <pebble.h>
+#include <stdlib.h>
+#include "time.c"
+  
 static Window *window;
 
 static TextLayer *temperature_layer;
@@ -22,6 +25,27 @@ static uint32_t WEATHER_ICONS[] = {
   RESOURCE_ID_IMAGE_RAIN,
   RESOURCE_ID_IMAGE_SNOW
 };
+static TextLayer *s_time_layer;
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00:00:00";
+
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    // Use 24 hour format
+    strftime(buffer, sizeof("00:00:00"), "%H:%M:%S", tick_time);
+  } else {
+    // Use 12 hour format
+    strftime(buffer, sizeof("00:00:00"), "%I:%M:%S", tick_time);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
 
 static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
   APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
@@ -48,7 +72,7 @@ static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tup
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
 
-  icon_layer = bitmap_layer_create(GRect(32, 10, 80, 80));
+  icon_layer = bitmap_layer_create(GRect(32, 20, 80, 80));
   layer_add_child(window_layer, bitmap_layer_get_layer(icon_layer));
 
   temperature_layer = text_layer_create(GRect(0, 100, 144, 68));
@@ -66,6 +90,16 @@ static void window_load(Window *window) {
       sync_tuple_changed_callback, sync_error_callback, NULL);
 
   layer_add_child(window_layer, text_layer_get_layer(temperature_layer));
+  
+  s_time_layer = text_layer_create(GRect(-2, 5,  155, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00:00");
+  
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
 
 static void window_unload(Window *window) {
@@ -78,7 +112,9 @@ static void window_unload(Window *window) {
   text_layer_destroy(temperature_layer);
   bitmap_layer_destroy(icon_layer);
 }
-
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
 static void init() {
   window = window_create();
   window_set_background_color(window, GColorBlack);
@@ -93,8 +129,14 @@ static void init() {
   app_message_open(inbound_size, outbound_size);
 
   const bool animated = true;
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
   window_stack_push(window, animated);
+  
+  //window_stack_push(s_main_window, true);
+  update_time();
 }
+
 
 static void deinit() {
   window_destroy(window);
